5757003e5b4bad5d26bae02a3d20c599d9193895
Klevakov <dklevakov@gmail.com>
2021-08-24 19:38:55 +0300

    Implemented user authorization

--- account/views.py
+++ account/views.py
@@ -2,14 +2,17 @@
 Модуль, содержащий представления.
 Представление – это функция Python, которая принимает Web-запрос и возвращает Web-ответ.
 """
-
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+
+
 from .forms import LoginForm
 
 
-def user_login(request):
+def user_login(request):  # pylint: disable=inconsistent-return-statements
     """
     Функция-представление - обработчик авторизации.
     При получении GET запроса - возвращает пустую форум авторизации.
@@ -17,21 +20,26 @@ def user_login(request):
     """
     # pylint: disable=no-else-return
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data  # pylint: disable=invalid-name
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd['password'])
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return HttpResponse('Authenticated successfully')
+            username = cd["username"]
+            password = cd["password"]
+            try:
+                User.objects.get(username=username)
+            except ObjectDoesNotExist:
+                return HttpResponse("The username you have entered does not exist.")
             else:
-                return HttpResponse('Disabled account')
-        else:
-            return HttpResponse('Invalid login')
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    if user.is_active:
+                        login(request, user)
+                        return HttpResponse("Authenticated successfully")
+                    else:
+                        return HttpResponse("Disabled account")
+                else:
+                    return HttpResponse("Invalid password. Try again.")
     else:
         form = LoginForm()
-        return render(request, 'account/login.html', {'form': form})
+        return render(request, "account/login.html", {"form": form})
